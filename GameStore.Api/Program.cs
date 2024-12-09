7eff3ee9aa3b2fb58c8153b251ee00161d20ef0a
DanielGallegosLB <daniel.gallegoslb@gmail.com>
2024-12-09 16:32:40 -0300

    gamesPost

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,43 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+const string GetGameEndpointName = "GetGame";
+
+List<GamesDto> games = [
+    new (
+        1,
+        "The Legend of Zelda: Breath of the Wild",
+        "Action-adventure",
+        59.99m, 
+        new DateOnly(2017, 3, 3)
+    ),
+    new (
+        2,
+        "Super Mario Odyssey",
+        "Platformer",
+        59.99m,
+        new DateOnly(2017, 10, 27)
+    ),
+    new (
+        3,
+        "Mario Kart 8 Deluxe",
+        "Racing",
+        59.99m,
+        new DateOnly(2017, 4, 28)
+    )];
+
+app.MapGet("/games", () => games);
+
+app.MapGet("/games/{id}", (int id) => games.FirstOrDefault(g => g.Id == id) is GamesDto game ? Results.Ok(game) : Results.NotFound())
+.WithName(GetGameEndpointName);
+
+app.MapPost("/games", (CreateGameDto game) => {
+    var newGame = new GamesDto(games.Count + 1, game.Name, game.Genre, game.Price, game.ReleaseDate);
+    games.Add(newGame);
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = newGame.Id }, newGame);
+});
 
 app.MapGet("/", () => "Hello World!");
 
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce72bc73f48fe66ccedc0495a4fdabb6b623671e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
